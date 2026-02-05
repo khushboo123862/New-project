ede98d01c15dc7daa6af2005f9d0c4c9689606ad
khushboo123862 <kk9115165@gmail.com>
2026-02-05 16:25:37 +0530

    i made a link

--- basicreact/vitereact/src/main.jsx
+++ basicreact/vitereact/src/main.jsx
@@ -1,9 +1,40 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+function MyApp(){
+  return(
+    <div>
+      <h1>custom App</h1>
+    </div>
+  )
+}
+
+/*const ReactElement ={
+    type: 'a',
+    props:{
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    Children: 'Click me to visit google'
+}  */
+
+    const anotherElement = (
+  <a href="https://google.come" target='_blank'>visit google</a>
+
+    
+)
+
+const reactElement = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+ 'click me to visit google'
+  
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  
+    reactElement
+  
 )
